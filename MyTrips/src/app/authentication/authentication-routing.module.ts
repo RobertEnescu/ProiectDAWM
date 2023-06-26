a2b04b4dadd991a8aab70df92558f10fb058d3e8
Robert <enescurobert88@gmail.com>
2023-06-26 20:04:45 +0300

    Finished Login+Register using Json Server

--- MyTrips/src/app/authentication/authentication-routing.module.ts
+++ MyTrips/src/app/authentication/authentication-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../helpers/auth.guard';
+import { TabelListaComponent } from '../tabel-lista/tabel-lista.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'tabel', component: TabelListaComponent, canActivate: [AuthGuard] }, //de schimbat cu noul main page
   { path: '**', redirectTo: 'login' },
 ];
 
